cb36a000db3cf9626d6ce52835b7b5cfe005fae4
HedvikaCapkova <hedvika.capkova@gmail.com>
2023-11-30 18:36:55 +0100

    lekce 11

---
+++ 231130_lekce_11_ESLint/es-sandbox/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2015: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'no-undef': 'error',
+    quotes: ['error', 'single'],
+    semi: 'error',
+  },
+};
